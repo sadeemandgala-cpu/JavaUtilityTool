c4bf7a868ee59f0c8bd8e40bcea486476c8cbbd3
sadeemandgala-cpu <sadeemandgala@gmail.com>
2025-12-01 18:06:00 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 
 public class Calculator {
 
+    // Basic addition method
     public int add(int a, int b) {
         return a + b;
     }
-
-    // ملاحظة: لا يوجد Factor ولا Validation هنا
-    // فقط الكود الأساسي
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
@@ -16,23 +14,14 @@ public class Calculator {
         System.out.println("Java Utility Tool");
         System.out.println("1. Add\n2. Exit");
 
-        while (true) {
-            System.out.print("Choose option: ");
-            int choice = sc.nextInt();
+        System.out.print("Choose option: ");
+        int choice = sc.nextInt();
 
-            if (choice == 2) break;
-
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter two numbers: ");
-                    int a = sc.nextInt();
-                    int b = sc.nextInt();
-                    System.out.println("Result: " + calc.add(a, b));
-                    break;
-
-                default:
-                    System.out.println("Invalid option!");
-            }
+        if (choice == 1) {
+            System.out.print("Enter two numbers: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Result: " + calc.add(a, b));
         }
 
         sc.close();
